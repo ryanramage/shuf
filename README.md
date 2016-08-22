22dc934c5b7d26e40bdcfe726dca5633ac9968e6
Ryan Ramage <ryan.ramage@gmail.com>
2016-08-22 09:07:09 -0600

    better modes

--- README.md
+++ README.md
@@ -6,8 +6,27 @@ based off https://www.gnu.org/software/coreutils/manual/html_node/shuf-invocatio
 
 ## CLI Usage
 
+### randomly output lines to stdout
+
     npm i shuf -g
     cat file | shuf
+
+### split into two files, with second limited to 15 lines telling shuf total initial lines (35410)
+
+    ®[~]$ wc -l -c ~/Downloads/log2.txt
+      35410  941848 /Users/ryan/Downloads/log2.txt
+    ®[~]$ cat ~/Downloads/log2.txt  | shuf  a.out b.out --n=15 --lc=35410
+
+
+### split into two files, with second limited to 15 lines.
+
+Less random, as more preference is given to earlier lines
+
+    ®[~]$ cat ~/Downloads/log2.txt  | shuf  a.out b.out --n=15
+
+### split into n files, with equal probablity of going to each file
+
+    ®[~]$ cat ~/Downloads/log2.txt  | shuf  a.out b.out c.out
 
 ## License
 
--- bin/cli.js
+++ bin/cli.js
@@ -1,7 +1,85 @@
 #!/usr/bin/env node
+var fs = require('fs')
+var path = require('path')
 var split2 = require('split2')
+var sort = require('lodash.sortby')
+var range = require('lodash.range')
+var shuffle = require('lodash.shuffle')
+var config = require('rc')('shuf', {
+  n: null,
+  lc: null
+})
 
-process.stdin.pipe(split2())
-  .on('data', function (line) {
-    if (!!Math.floor(Math.random() * 2)) console.log(line)
+begin()
+
+function begin () {
+  if (config._.length === 0 && config.n === null) return mode_to_stdout()
+  if (config._.length === 2 && config.n === null) {
+    return mode_equal_dist()
+  }
+  if (config._.length !== 2 && config.n === null) {
+    return mode_equal_dist()
+  }
+  if (config._.length === 2 && config.n !== null) {
+    var a = fs.createWriteStream(path.resolve(config._[0]))
+    var b = fs.createWriteStream(path.resolve(config._[1]))
+    if (config.lc) return mode_two_file_with_limit_and_lc(a, b, config.n, config.lc)
+    else return mode_two_file_with_limit(a, b, config.n)
+  }
+  if (config._.length !== 2 && config.n !== null) {
+    console.log('Error, invalid mode. n option only available with 2 files')
+    return usage()
+  }
+}
+function mode_to_stdout () {
+  process.stdin.pipe(split2())
+    .on('data', function (line) {
+      if (!!Math.floor(Math.random() * 2)) console.log(line)
+    })
+}
+
+function mode_two_file_with_limit (a, b, limit) {
+  var count = 0
+  process.stdin.pipe(split2())
+    .on('data', function (line) {
+      var target = a
+      if (count < limit) {
+        if (!!Math.floor(Math.random() * 2)) {
+          target = b
+          count++
+        }
+      }
+      target.write(line + '\n')
+    })
+}
+
+function mode_two_file_with_limit_and_lc (a, b, limit, line_count) {
+  var count = 0
+  var numbers = sort(shuffle(range(0, line_count)).slice(0, limit))
+  process.stdin.pipe(split2())
+    .on('data', function (line) {
+      if (count === numbers[0]) {
+        b.write(line + '\n')
+        numbers.shift()
+      }
+      else a.write(line + '\n')
+      count++
+    })
+}
+
+function mode_equal_dist () {
+  var output = []
+  config._.forEach(function (file) {
+    output.push(fs.createWriteStream(path.resolve(file)))
   })
+  process.stdin.pipe(split2())
+    .on('data', function (line) {
+      var random_index = Math.floor(Math.random() * output.length)
+      var target = output[random_index]
+      target.write(line + '\n')
+    })
+}
+
+function usage () {
+  console.log('cat file.txt | shuf [-n] out1.txt out2.txt')
+}
